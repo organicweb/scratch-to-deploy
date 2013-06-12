a325dcbb2877faa7035a78b743cbb57da97b3cde
Mathias <contact@organicweb.fr>
2013-06-12 14:05:17 +0200

    all

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Std::Application.config.secret_token = '339ffbfc7079bbb9075153321256441f6a416fb2fb942ab5f40e743e98d79f9618b1b94cbb6f2f9207d8870aa867c5c459aeb26a2c7318c2994c3830239d43da'
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Std::Application.routes.draw do
+  devise_for :users
+
+  resources :posts
+
+
+  
+  root :to => 'posts#index'
+
+  
+end
